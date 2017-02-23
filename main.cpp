6e9168ca660aab3b9b89539e28be0fce138fe3d6
Jack Wilson <wilsonja@flip2.engr.oregonstate.edu>
2017-02-22 20:23:42 -0800

    refactored peg structs to hold vector of all moves

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 struct peg{
 	int id;
 	int pos;
-	bool on;
-
-	peg(int x, bool y);
-//	bool complete(vector<peg> v);
-//	void hop(peg x, peg y);
+	int on;
+	vector<int> jump;
+	vector<int> land;
+	
+	peg(int x, int y, int z);
 };
 
 bool complete(vector<peg> v){
@@ -29,123 +29,124 @@ bool complete(vector<peg> v){
 		return false;
 	}
 }
-/*
-void peg::hop(peg x, peg y){
-	x.pos = y.pos;
-	y.pos = 0;
-	y.on = 0;
-}*/
 
-peg::peg(int x, bool y, int z){
+peg::peg(int x, int y, int z){
 		pos = x;
 		on = y;
 		id = z;
 }
 
 void printBoard(vector<peg> v){
-	for(int i = 0; i<v.size(); i++){
-		if(v[i].on){
-			printf("%d, \n", v[i].id);
-		}
-	}
+//	for(int i = 0; i<v.size(); i++){
+//		printf("
+//	}
+	printf("         %d\n", v[0].on);
+	printf("        %d %d\n", v[1].on, v[2].on);
+	printf("       %d %d %d\n", v[3].on, v[4].on, v[5].on);
+	printf("      %d %d %d %d\n", v[6].on, v[7].on, v[8].on, v[9].on);
+	printf("     %d %d %d %d %d\n", v[10].on, v[11].on, v[12].on, v[13].on, v[14].on);
 }
 
 void sort(vector<peg> v){
-	if(complete(v)){
-		printBoard(v);
-	}
-	else{
-		for(int i=0; i<v.size(); i++){
-			if(v[i].on){
-				if(v[i].pos == 1){
-					if((v[2].on) && !(v[4].on)){
-						v[i].pos = v[4].pos;
-						v[2].on = false;	
-						sort(v);
-					}
-					if(v[3].on && !(v[6].on)){
-						v[i].pos = v[6].pos
-						v[3].on = false;
-						sort(v);
-					}
-				}
-				if(v[i].pos == 2){
-					if(v[4].on && !(v[7].on)){
-						v[i].pos = v[7].pos//will this mess things up?
-						v[4].on = false;
-						sort(v);
-					}
-					if(v[5].on && !(v[9].on)){
-						v[i].pos = v[9].pos
-						v[5].on = false;
-						sort(v);
-					}
-				}
-				if(v[i].pos == 3){
-					if(v[5].on && !(v[8].on)){
-						v[i].pos = v[8].pos
-						v[5].on = false;
-						sort(v);
-					}
-					if(v[6].on && !(v[10].on)){
-						v[i].pos = v[10].pos;
-						v[6].on = false;
-						sort(v);
-					}
-				}
-				if(v[i].pos == 4){
-					if(v[2].on && !(v[1].on)){
-						v[i].pos = v[1].pos;
-						v[2].on = false;
-						sort(v):
-					}
-					if(v[5].on && !(v[6].on)){
-						v[i].pos = v[6].pos;
-						v[5].on = false;
-						sort(v):
-					}
-					if(v[8].on && !(v[13].on)){
-						v[i].pos = v[13].pos;
-						v[8].on = false;
-						sort(v);
-					}
-					if(v[7].on && !(v[11].on)){
-						v[i].pos = v[11].pos;
-						v[7].on = false;
-						sort(v);
-					}
-				}
-				if
-
-			}
-		}
-	}
+	
 }
 
 
 
 int main(int argc, char* argv[]){
 	peg one(1, 1, 1);
+	one.jump.push_back(2);
+	one.jump.push_back(3);
+	one.land.push_back(4);
+	one.land.push_back(6);
 	peg two(2, 1, 2);
+	two.jump.push_back(4);
+	two.jump.push_back(5);
+	two.land.push_back(7);
+	two.land.push_back(9);
 	peg three(3, 1, 3);
+	three.jump.push_back(5);
+	three.jump.push_back(6);
+	three.land.push_back(8);
+	three.land.push_back(10);
 	peg four(4, 1, 4);
+	four.jump.push_back(2);
+	four.jump.push_back(5);
+	four.jump.push_back(7);
+	four.jump.push_back(8);
+	four.land.push_back(1);
+	four.land.push_back(6);
+	four.land.push_back(11);
+	four.land.push_back(13);
 	peg five(5, 1, 5);
+	five.jump.push_back(8);
+	five.jump.push_back(9);
+	five.land.push_back(12);
+	five.land.push_back(14);
 	peg six(6, 1, 6);
+	six.jump.push_back(3);
+	six.jump.push_back(5);
+	six.jump.push_back(9);
+	six.jump.push_back(10);
+	six.land.push_back(1);
+	six.land.push_back(4);
+	six.land.push_back(13);
+	six.land.push_back(15);
 	peg seven(7, 1, 7);
+	seven.jump.push_back(4);
+	seven.jump.push_back(8);
+	seven.land.push_back(2);
+	seven.land.push_back(9);
 	peg eight(8, 1, 8);
+	eight.jump.push_back(5);
+	eight.jump.push_back(9);
+	eight.land.push_back(3);
+	eight.land.push_back(10);
 	peg nine(9, 1, 9);
+	nine.jump.push_back(5);
+	nine.jump.push_back(8);
+	nine.land.push_back(2);
+	nine.land.push_back(7);
 	peg ten(10, 1, 10);
+	ten.jump.push_back(6);
+	ten.jump.push_back(9);
+	ten.land.push_back(3);
+	ten.land.push_back(8);
 	peg eleven(11, 1, 11);
+	eleven.jump.push_back(7);
+	eleven.jump.push_back(12);
+	eleven.land.push_back(4);
+	eleven.land.push_back(13);
 	peg twelve(12, 1, 12);
+	twelve.jump.push_back(8);
+	twelve.jump.push_back(13);
+	twelve.land.push_back(5);
+	twelve.land.push_back(14);
 	peg thirt(13, 1, 13);
+	thirt.jump.push_back(8);
+	thirt.jump.push_back(9);
+	thirt.jump.push_back(12);
+	thirt.jump.push_back(14);
+	thirt.land.push_back(4);
+	thirt.land.push_back(6);
+	thirt.land.push_back(11);
+	thirt.land.push_back(15);
 	peg fourt(14, 1, 14);
+	fourt.jump.push_back(9);
+	fourt.jump.push_back(13);
+	fourt.land.push_back(5);
+	fourt.land.push_back(12);
 	peg fift(15, 1, 15);
+	fift.jump.push_back(10);
+	fift.jump.push_back(14);
+	fift.land.push_back(6);
+	fift.land.push_back(13);
 
 	vector<peg> v;
 	v.push_back(one);
 	v.push_back(two);
 	v.push_back(three);
-/*	v.push_back(four);
+	v.push_back(four);
 	v.push_back(five);
 	v.push_back(six);
 	v.push_back(seven);
@@ -157,7 +158,7 @@ int main(int argc, char* argv[]){
 	v.push_back(thirt);
 	v.push_back(fourt);
 	v.push_back(fift);
-*/
+
 	printBoard(v);
 	sort(v);
 	return 0;
